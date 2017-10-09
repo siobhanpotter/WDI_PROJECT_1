255e20bbdfcd0b0ae04c8acb2ecda52125b16260
siobhanpotter <siobhanpotterone@gmail.com>
2017-10-09 12:42:38 +0100

    fixed randomize letters function, created checkAnswer function

--- js/app.js
+++ js/app.js
@@ -23,6 +23,7 @@ function randomizeLetters() {
 };
 
 function randomizeLettersloop() {
+  //here is where the checkAnswer would be located, I think.
   for (var i = 0; i < jumbledWordsArray.length; i++) {
     jumbledWordsArray[i]
     randomizeLetters();
@@ -54,10 +55,12 @@ $button.on('click', function() {
   randomizeLetters();
 });
 
-$submitButton.on('click', function() {
-  let $userInput = $input.val();
+$submitButton.on('click', function(e) {
+  e.preventDefault();
+  $userInput = $input.val();
+
+  // console.lo g($userInput);
   checkAnswer();
-  console.log($userInput);
 });
 
 
@@ -67,9 +70,13 @@ function checkAnswer() {
   //If the first word the user enters into the imput box is the same as the first word in the wordsArray alert 'right answer', and that word dissapears, if it is different alert 'keep trying'.
 
   // If the contents of the imput box is the same as wordsArray[i], make the word dissapear / display none, alert the user they have got the correct answer, allow the user to move on to the next word.
-  if($userInput === jumbledWord[0]){
-    console.log('its working');
-  }
+  if($userInput === wordsArray[0]) {
+    alert('correct answer');
+    // console.log(`yay ${wordsArray[0]}`);
+  } else {
+      alert('keep trying');
+    }
+
 }
 
 
